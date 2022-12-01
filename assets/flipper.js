7fa8d7c9f9545cfc9096084ab989dabb9188847b
maiconsdk <contato@maiconsilva.com.br>
2022-11-30 23:38:49 -0300

    Refatorando js para iniciar após load da window.

--- assets/flipper.js
+++ assets/flipper.js
@@ -1,14 +1,16 @@
-const changeButton = document.getElementById('flipper__change-btn-js');
-let currentColor = document.getElementById('flipper__current-color-js');
+window.onload = () => {
+  const changeButton = document.getElementById('flipper__change-btn-js');
+  let currentColor = document.getElementById('flipper__current-color-js');
 
-function generateNewColor() {
-  return `#${parseInt((Math.random() * 0xFFFFFF)).toString(16).padStart(6, '0')}`;
+  function generateNewColor() {
+    return `#${parseInt((Math.random() * 0xFFFFFF)).toString(16).padStart(6, '0')}`;
+  }
+
+  function getNewColor() {
+    let color = generateNewColor();
+    document.body.style.backgroundColor = color;
+    currentColor.innerText = color;
+  }
+
+  changeButton.addEventListener('click', getNewColor);
 }
-
-function getNewColor() {
-  let color = generateNewColor();
-  document.body.style.backgroundColor = color;
-  currentColor.innerText = color;
-}
-
-changeButton.addEventListener('click', getNewColor);
